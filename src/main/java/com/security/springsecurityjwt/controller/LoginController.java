ab26a5fb800761371b04770b62361c9d305b718d
zhangguangnian <69832745+zgn19950209@users.noreply.github.com>
2022-10-08 20:14:58 +0800

    Changes

--- src/main/java/com/security/springsecurityjwt/controller/LoginController.java
+++ src/main/java/com/security/springsecurityjwt/controller/LoginController.java
@@ -1,10 +1,8 @@
 package com.security.springsecurityjwt.controller;
 
-import lombok.var;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
